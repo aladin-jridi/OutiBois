e08dd028bddb7451c6236da63018595b946a9795
aladin-jridi <aladinjridi007@gmail.com>
2022-02-22 15:34:05 +0100

    login admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import './AdminLogin'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import AdminLogin from './AdminLogin'
 function App() {
 	return (
 		<BrowserRouter>
 		<Routes>
-			<Route path = '/Admin' element={<AdminLogin/>} ></Route>
+		<Route path = '/Admin' element={<AdminLogin/>} ></Route>
+
 		</Routes>
 		</BrowserRouter>
 	)
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-// import Login from './AdminLogin'
+import AdminLogin from './AdminLogin'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/* <Login/> */}
+    <AdminLogin/>
   </React.StrictMode>,
   document.getElementById('root')
 )
