05a9d9514c5bd8fd961b0847bfa3f0e9d34301f7
aladin-jridi <aladinjridi007@gmail.com>
2022-02-23 23:27:42 +0100

    admin with map

--- src/AdminDASH.jsx
+++ src/AdminDASH.jsx
@@ -1,28 +1,44 @@
-function AdminDADH() {
+import React, { useState, useEffect,useLayoutEffect } from "react";
+import axios from "axios";
 
-    $(document).ready(function(){
-        // Activate tooltip
-        $('[data-toggle="tooltip"]').tooltip();
-        
-        // Select/Deselect checkboxes
-        var checkbox = $('table tbody input[type="checkbox"]');
-        $("#selectAll").click(function(){
-            if(this.checked){
-                checkbox.each(function(){
-                    this.checked = true;                        
-                });
-            } else{
-                checkbox.each(function(){
-                    this.checked = false;                        
-                });
-            } 
-        });
-        checkbox.click(function(){
-            if(!this.checked){
-                $("#selectAll").prop("checked", false);
-            }
-        });
-    });
+function AdminDADH() {
+  // $(document).ready(function () {
+  //   // Activate tooltip
+  //   $('[data-toggle="tooltip"]').tooltip();
+
+  //   // Select/Deselect checkboxes
+  //   var checkbox = $('table tbody input[type="checkbox"]');
+  //   $("#selectAll").click(function () {
+  //     if (this.checked) {
+  //       checkbox.each(function () {
+  //         this.checked = true;
+  //       });
+  //     } else {
+  //       checkbox.each(function () {
+  //         this.checked = false;
+  //       });
+  //     }
+  //   });
+  //   checkbox.click(function () {
+  //     if (!this.checked) {
+  //       $("#selectAll").prop("checked", false);
+  //     }
+  //   });
+  // });
+
+  const [newMachines, setnewMachines] = useState([]);
+
+  useLayoutEffect(() => {
+    axios
+      .get("http://localhost:5000/api/newMachine/findAllMachine")
+      .then(({ data }) => {
+        console.log(data);
+        setnewMachines(data);
+      });
+    },[]);
+  useEffect(()=>{
+    console.log(newMachines);
+  })
 
   return (
     <div className="10">
@@ -56,251 +72,74 @@ function AdminDADH() {
                 </div>
               </div>
             </div>
-            <table className="table table-striped table-hover">
-              <thead>
-                <tr>
-                  <th>
-                    <span className="custom-checkbox">
-                      <input type="checkbox" id="selectAll" />
-                      <label htmlFor="selectAll"></label>
-                    </span>
-                  </th>
-                  <th>Name of Machine</th>
-                  <th>Email</th>
-                  <th>Address</th>
-                  <th>Phone</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <span className="custom-checkbox">
-                      <input
-                        type="checkbox"
-                        id="checkbox1"
-                        name="options[]"
-                        value="1"
-                      />
-                      <label htmlFor="checkbox1"></label>
-                    </span>
-                  </td>
-                  <td>Thomas Hardy</td>
-                  <td>thomashardy@mail.com</td>
-                  <td>89 Chiaroscuro Rd, Portland, USA</td>
-                  <td>(171) 555-2222</td>
-                  <td>
-                    <a
-                      href="#editEmployeeModal"
-                      className="edit"
-                      data-toggle="modal"
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Edit"
-                      >
-                        &#xE254;
-                      </i>
-                    </a>
-                    <a
-                      href="#deleteEmployeeModal"
-                      className="delete"
-                      data-toggle="modal"
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Delete"
-                      >
-                        &#xE872;
-                      </i>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <span className="custom-checkbox">
-                      <input
-                        type="checkbox"
-                        id="checkbox2"
-                        name="options[]"
-                        value="1"
-                      />
-                      <label htmlFor="checkbox2"></label>
-                    </span>
-                  </td>
-                  <td>Dominique Perrier</td>
-                  <td>dominiqueperrier@mail.com</td>
-                  <td>Obere Str. 57, Berlin, Germany</td>
-                  <td>(313) 555-5735</td>
-                  <td>
-                    <a
-                      href="#editEmployeeModal"
-                      className="edit"
-                      data-toggle="modal"
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Edit"
-                      >
-                        &#xE254;
-                      </i>
-                    </a>
-                    <a
-                      href="#deleteEmployeeModal"
-                      className="delete"
-                      data-toggle="modal"
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Delete"
-                      >
-                        &#xE872;
-                      </i>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <span className="custom-checkbox">
-                      <input
-                        type="checkbox"
-                        id="checkbox3"
-                        name="options[]"
-                        value="1"
-                      />
-                      <label htmlFor="checkbox3"></label>
-                    </span>
-                  </td>
-                  <td>Maria Anders</td>
-                  <td>mariaanders@mail.com</td>
-                  <td>25, rue Lauriston, Paris, France</td>
-                  <td>(503) 555-9931</td>
-                  <td>
-                    <a
-                      href="#editEmployeeModal"
-                      className="edit"
-                      data-toggle="modal"
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Edit"
-                      >
-                        &#xE254;
-                      </i>
-                    </a>
-                    <a
-                      href="#deleteEmployeeModal"
-                      className="delete"
-                      data-toggle="modal"
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Delete"
-                      >
-                        &#xE872;
-                      </i>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <span className="custom-checkbox">
-                      <input
-                        type="checkbox"
-                        id="checkbox4"
-                        name="options[]"
-                        value="1"
-                      />
-                      <label htmlFor="checkbox4"></label>
-                    </span>
-                  </td>
-                  <td>Fran Wilson</td>
-                  <td>franwilson@mail.com</td>
-                  <td>C/ Araquil, 67, Madrid, Spain</td>
-                  <td>(204) 619-5731</td>
-                  <td>
-                    <a
-                      href="#editEmployeeModal"
-                      className="edit"
-                      data-toggle="modal"
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Edit"
-                      >
-                        &#xE254;
-                      </i>
-                    </a>
-                    <a
-                      href="#deleteEmployeeModal"
-                      className="delete"
-                      data-toggle="modal"
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Delete"
-                      >
-                        &#xE872;
-                      </i>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <span className="custom-checkbox">
-                      <input
-                        type="checkbox"
-                        id="checkbox5"
-                        name="options[]"
-                        value="1"
-                      />
-                      <label htmlFor="checkbox5"></label>
-                    </span>
-                  </td>
-                  <td>Martin Blank</td>
-                  <td>martinblank@mail.com</td>
-                  <td>Via Monte Bianco 34, Turin, Italy</td>
-                  <td>(480) 631-2097</td>
-                  <td>
-                    <a
-                      href="#editEmployeeModal"
-                      className="edit"
-                      data-toggle="modal"
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Edit"
-                      >
-                        &#xE254;
-                      </i>
-                    </a>
-                    <a
-                      href="#deleteEmployeeModal"
-                      className="delete"
-                      data-toggle="modal"
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Delete"
-                      >
-                        &#xE872;
-                      </i>
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-           
+            <div>
+                <table className="table table-striped table-hover">
+                  <thead>
+                    <tr>
+                      <th>
+                        <span className="custom-checkbox">
+                          <input type="checkbox" id="selectAll" />
+                          <label htmlFor="selectAll"></label>
+                        </span>
+                      </th>
+                      <th>Name of Machine</th>
+                      <th>image</th>
+                      <th>discription</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {newMachines.map((machine,index)=>(
+                       <tr key={index}>
+                       <td>
+                         <span className="custom-checkbox">
+                           <input
+                             type="checkbox"
+                             id="checkbox1"
+                             name="options[]"
+                             value="1"
+                           />
+                           <label htmlFor="checkbox1"></label>
+                         </span>
+                       </td>
+                       <td>{machine.name}</td>
+                       <td><img className="machine-image-admine-bord" style={{width:100,height:100}} src={machine.image[0]}/></td>
+                       <td>{machine.discription}</td>
+                       <td>
+                         <a
+                           href="#editEmployeeModal"
+                           className="edit"
+                           data-toggle="modal"
+                         >
+                           <i
+                             className="material-icons"
+                             data-toggle="tooltip"
+                             title="Edit"
+                           >
+                             &#xE254;
+                           </i>
+                         </a>
+                         <a
+                           href="#deleteEmployeeModal"
+                           className="delete"
+                           data-toggle="modal"
+                         >
+                           <i
+                             className="material-icons"
+                             data-toggle="tooltip"
+                             title="Delete"
+                           >
+                             &#xE872;
+                           </i>
+                         </a>
+                       </td>
+                     </tr>
+                    ))}
+                   
+                  </tbody>
+                </table>;
+              
+            </div>
           </div>
         </div>
       </div>
@@ -310,7 +149,7 @@ function AdminDADH() {
           <div className="modal-content">
             <form>
               <div className="modal-header">
-                <h4 className="modal-title">Add Employee</h4>
+                <h4 className="modal-title">Add Machines</h4>
                 <button
                   type="button"
                   className="close"
@@ -326,15 +165,12 @@ function AdminDADH() {
                   <input type="text" className="form-control" required />
                 </div>
                 <div className="form-group">
-                  <label>Email</label>
-                  <input type="email" className="form-control" required />
+                  <label>Image</label>
+                  <input type="file" className="form-control" required />
                 </div>
+              
                 <div className="form-group">
-                  <label>Address</label>
-                  <textarea className="form-control" required></textarea>
-                </div>
-                <div className="form-group">
-                  <label>Phone</label>
+                  <label>discription</label>
                   <input type="text" className="form-control" required />
                 </div>
               </div>
@@ -345,7 +181,7 @@ function AdminDADH() {
                   data-dismiss="modal"
                   value="Cancel"
                 />
-                <input type="submit" className="btn btn-success" value="Add" />
+                <button type="submit" className="btn btn-success" >Add</button>
               </div>
             </form>
           </div>
@@ -437,10 +273,7 @@ function AdminDADH() {
           </div>
         </div>
       </div>
-      
     </div>
-
-    
   );
 }
 
