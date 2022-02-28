69a9073bfe4f2aac16c5c9954050a353e1c5db6d
sakrbelgyesmi <belguesmisakr@gmail.com>
2022-02-28 17:21:06 +0100

    cloudinary upload image with bad request error

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import "./App.css";
 import "./Admin.css";
 import AdminLogin from "./AdminLogin";
 import AdminDADH from "./AdminDASH";
-
+import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-		<Route path='/' element={<Home />}></Route>
-        <Route path="/Admin" element={<AdminLogin />}></Route>
-        <Route path="/AdminDADH" element={<AdminDADH />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/admin' element={<AdminLogin />}></Route>
+				<Route path='/Admindash' element={<AdminDADH />}></Route>
+				<Route path='/' element={<Home />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
