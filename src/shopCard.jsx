89515619254e88a7474b51b2a89275536128e0bd
wassimelfen <wassimelfen@gmail.com>
2022-03-06 00:40:11 +0100

    styling

--- src/shopCard.jsx
+++ src/shopCard.jsx
@@ -2,50 +2,54 @@ import React, { useState } from "react";
 import { Image } from "cloudinary-react";
 
 const ShopCard = ({ shopcard, removeFromCard }) => {
-	const [toggleIdShopCard, settoggleIdShopCard] = useState("");
-	const [toggleIdForm, settoggleIdForm] = useState("display-none");
-	return (
-		<div className='module module-cart pull-left'>
-			<div className='cart-icon'>
-				<i className='fa fa-shopping-cart' />
-				<span className='title'>shop cart</span>
-				<span className='cart-label'>{shopcard.length}</span>
-			</div>
-			<div className='cart-box' id={toggleIdShopCard}>
-				<div className='cart-overview'>
-					<ul className='list-unstyled'>
-						{shopcard.map((machine, index) => (
-							<li key={index}>
-								<Image
-									className='img-responsive'
-									cloudName='outibois'
-									public_id={machine.image[0]}
-								/>
-								<div className='product-meta'>
-									<h5 className='product-title'>
-										{machine.name}
-									</h5>
-								</div>
-								<a
-									className='cancel'
-									onClick={() => removeFromCard(machine)}
-								>
-									cancel
-								</a>
-							</li>
-						))}
-					</ul>
-				</div>
-				<div className='clearfix'></div>
-				<div className='cart-control'>
-					<a className='btn btn-secondary pull-right'>Devis</a>
-				</div>
-			</div>
-			<div className='cart-box' id={toggleIdForm}>
+  const [toggleIdShopCard, settoggleIdShopCard] = useState("");
+  const [toggleIdForm, settoggleIdForm] = useState("display-none");
+  return (
+    <div className="module module-cart pull-left">
+      <div className="cart-icon">
+        <i className="fa fa-shopping-cart" />
+        <span className="cart-label">{shopcard.length}</span>
+      </div>
 
-			</div>
-		</div>
-	);
+      {/* <table className="cart-icon">
+        <tr>
+          <td>
+            <i className="fa fa-shopping-cart" />
+          </td>
+          <td>
+            <span className="cart-label">{shopcard.length}</span>
+          </td>
+        </tr>
+      </table> */}
+
+      <div className="cart-box" id={toggleIdShopCard}>
+        <div className="cart-overview">
+          <ul className="list-unstyled">
+            {shopcard.map((machine, index) => (
+              <li key={index}>
+                <Image
+                  className="img-responsive"
+                  cloudName="outibois"
+                  public_id={machine.image[0]}
+                />
+                <div className="product-meta">
+                  <h5 className="product-title">{machine.name}</h5>
+                </div>
+                <a className="cancel" onClick={() => removeFromCard(machine)}>
+                  cancel
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="clearfix"></div>
+        <div className="cart-control">
+          <a className="btn btn-secondary pull-right">Devis</a>
+        </div>
+      </div>
+      <div className="cart-box" id={toggleIdForm}></div>
+    </div>
+  );
 };
 
 export default ShopCard;
