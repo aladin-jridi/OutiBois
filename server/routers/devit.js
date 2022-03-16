d809f436aa84df1a02952c51c008e9582ccb969b
sakrbelgyesmi <belguesmisakr@gmail.com>
2022-03-16 16:53:20 +0100

    style modofocation

--- server/routers/devit.js
+++ server/routers/devit.js
@@ -12,7 +12,7 @@ router.route("/findManydevit").get(devit.add_many_devit);
 
 router.route("/updateOnedevit/:_id").put(devit.update_one_devit);
 
-router.route("/deleteOnedevit/:_id").delete(devit.delete_one_devit);
+router.route("/deleteOne/:_id").delete(devit.delete_one_devit);
 
 router.route("/findAll").get(devit.find_all_devit);
 
